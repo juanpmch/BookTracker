c69cbb392093ae9d984da170bafc985062359a2c
Juan <juan.mucholachaves@vdabcampus.be>
2026-07-06 13:22:22 +0200

    Add Book Entity

---
+++ BookTracker.Api/Domain/Book.cs
@@ -0,0 +1,8 @@
+namespace BookTracker.Api.Domain;
+public class Book
+{
+    public int Id { get; set; }
+    public required string Title { get; set; }
+    public required string Author { get; set; }
+    public int Year { get; set; }
+}
