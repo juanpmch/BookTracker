520ab3294e4050ee4c4d8f95b815b81484875ea1
Juan <juan.mucholachaves@vdabcampus.be>
2026-07-08 12:28:36 +0200

    Add EF Core + SQLite persistence with initial migration

--- BookTracker.Api/Program.cs
+++ BookTracker.Api/Program.cs
@@ -1,20 +1,35 @@
 using BookTracker.Api.Application;
 using BookTracker.Api.Storage;
 using BookTracker.Api.Application.CreateBook;
-
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddSingleton<IBookRepository, InMemoryBookRepository>();
+
+var connectionString = "Data Source=books.db";
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(connectionString));
+
+builder.Services.AddScoped<IBookRepository, EfBookRepository>();
 builder.Services.AddScoped<BookService>();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
 app.MapGet("/books", async (BookService service) => Results.Ok(await service.GetAllBooks()));
 app.MapPost("/books", async (CreateBookRequest request, BookService service) =>
 {
     var response = await service.CreateBook(request);
     return Results.Created($"/books/{response.Id}", response);
 });
+
 app.Run();
+
 public partial class Program;
-
-
