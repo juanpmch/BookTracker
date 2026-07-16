338c4602e0cbfdfcd83ce527b517dd3ec76c29a2
Juan <juan.mucholachaves@vdabcampus.be>
2026-07-16 11:42:40 +0200

    clean up program setup

--- BookTracker.Api/Program.cs
+++ BookTracker.Api/Program.cs
@@ -1,46 +1,18 @@
-
-using BookTracker.Api.Storage;
-using Microsoft.EntityFrameworkCore;
-using BookTracker.Api.Endpoints;
-using BookTracker.Api.Application.GetBookSummaries;
-using BookTracker.Api.Application.GetBookDetails;
-using BookTracker.Api.Application.CreateBook;
-using BookTracker.Api.Application.UpdateBook;
-using BookTracker.Api.Application.DeleteBook;
-using BookTracker.Api.Seeding;
+using BookTracker.Api.Wiring;
 
 var builder = WebApplication.CreateBuilder(args);
-
-var connectionString = "Data Source=books.db";
-
-builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlite(connectionString));
-
-builder.Services.AddScoped<IBookRepository, EfBookRepository>();
-
-builder.Services.AddScoped<GetBookSummariesQueryHandler>();
-builder.Services.AddScoped<GetBookDetailsQueryHandler>();
+builder.AddBookTracker();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<CreateBookCommandHandler>();
-builder.Services.AddScoped<UpdateBookCommandHandler>();
-builder.Services.AddScoped<DeleteBookCommandHandler>();
 
 var app = builder.Build();
+app.UseBookTracker();
 
 if (app.Environment.IsDevelopment())
 {
-    using (var scope = app.Services.CreateScope())
-    {
-        var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-
-        dbContext.Database.EnsureCreated();
-        if (builder.Configuration.GetValue<bool>("SeedDatabase"))
-            DatabaseSeeder.SeedBooks(dbContext, 500);
-    }
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
-
-app.MapBookEndpoints();
 
 app.Run();
 
